31d8a97af1b2105e9395741fc06290ab69114f0f
Christian Bayer <christian.bayer1979@gmail.com>
2021-06-15 15:26:03 +0200

    Add saving of figures.

--- analyze_resnet.py
+++ analyze_resnet.py
@@ -12,19 +12,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 nLayers = 128
-nEpochs = 10
+nEpochs = 100
+
+## save figures?
+saveP = True
 
 ##fNameBase = "resnet" + str(nLayers) + "_relu_lin_e" + str(nEpochs)
 fNameBase = f"resnet{nLayers}_relu_lin_e{nEpochs}"
 fName = fNameBase + ".pth"
 
-## Save the weights
+## Load the weights
 with open(fNameBase + "_weights.npy", 'rb') as f:
     dW = np.load(f)
+    
+with open(fNameBase + "_weights_cumsum.npy", 'rb') as f:
+    W = np.load(f) 
 
 ## Plot some sample paths
 plt.plot(dW[:,3,90])
+if saveP:
+    plt.savefig("./plots/"+fNameBase+f"increments_{3}_{90}.pdf")
 plt.show()
+
+## plot sample path of the process W
+plt.plot(W[:,3,90])
+if saveP:
+    plt.savefig("./plots/"+fNameBase+f"path_{3}_{90}.pdf")
+plt.show()
+
 
 ## Compute p-variations of 1-dimensional sample path
 iIndex = 0
@@ -35,6 +50,8 @@ with open(fName_i_j, "rb") as f:
     pVars_i_j = np.load(f)
     
 plt.plot(pGrid, pVars_i_j**(1.0/pGrid))
+if saveP:
+    plt.savefig("./plots/"+fNameBase+f"pVar_{iIndex}_{jIndex}.pdf")
 plt.show()
 
 ## Compute the p-variation for the whole matrix, using a specific matrix norm
@@ -46,8 +63,9 @@ with open(fNameFro, "rb") as f:
     pVarsFro = np.load(f)
         
 plt.plot(pGrid, pVarsFro**(1.0/pGrid))
+if saveP:
+    plt.savefig("./plots/"+fNameBase+"pVar_Fro.pdf")
 plt.show()
-## Gives an increasing function???
 
 mNorm = 2
 pGrid = np.linspace(1.0, 3.0, 100)
@@ -57,4 +75,6 @@ with open(fName2, "rb") as f:
     pVars2 = np.load(f)
         
 plt.plot(pGrid, pVars2**(1.0/pGrid))
+if saveP:
+    plt.savefig("./plots/"+fNameBase+"pVar_2.pdf")
 plt.show()
