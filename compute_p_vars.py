ef7211ebc8ba5769a77bf7074e016f58232c7f0e
Christian Bayer <christian.bayer1979@gmail.com>
2021-07-16 14:12:07 +0200

    Small changes in the organization of compute_p_vars.py.

--- compute_p_vars.py
+++ compute_p_vars.py
@@ -8,8 +8,10 @@ Read out the ResNet as trained in resnet.py and further analyze it.
 @author: bayerc
 """
 
+import os
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 from p_var import p_var_backbone, p_var_backbone_ref
 
 ## Check if the variations should be re-computed
@@ -23,49 +25,67 @@ fName = fNameBase + ".pth"
 ## To get parameters: resnet.resnet_stack[i].block[1].weight.data
 ## for the ith ResNet-block, i=1,.. (i=0 is a Flatten layer)
 
-resnet_dic = torch.load(fName)
-resnet_vals = list(resnet_dic.values())
-# now need to convert the parameters of Resnet blocks to numpy arrays
-dW_list = [resnet_vals[i].cpu().numpy() for i in range(1, nLayers)]
-dW = np.array(dW_list)
+if not os.path.isfile(fNameBase + "_weights.npy"):
+    resnet_dic = torch.load(fName)
+    resnet_vals = list(resnet_dic.values())
+    # now need to convert the parameters of Resnet blocks to numpy arrays
+    dW_list = [resnet_vals[i].cpu().numpy() for i in range(1, nLayers)]
+    dW = np.array(dW_list)
+    W = np.cumsum(dW, axis=0)
+    
+    ## Save the weights
+    with open(fNameBase + "_weights.npy", 'wb') as f:
+        np.save(f, dW)
+        np.save(f, W)
+else:
+    with open(fNameBase + "_weights.npy", 'rb') as f:
+        dW = np.load(f)
+        W = np.load(f)
 
-## Save the weights
-with open(fNameBase + "_weights.npy", 'wb') as f:
-    np.save(f, dW)
-
-## Now compute the path W
-W = np.cumsum(dW, axis=0)
-with open(fNameBase + "_weights_cumsum.npy", 'wb') as f:
-    np.save(f, W)
-
-## Compute p-variations of 1-dimensional sample path
 iIndex = 0
 jIndex = 0
 pGrid = np.linspace(1.0, 3.0, 100)
-path_dist = lambda k, l: np.abs(W[k,iIndex,jIndex] - W[l,iIndex,jIndex])
-fName_i_j = fNameBase + f"_pVar_{iIndex}_{jIndex}.npy"
 
-pVars_i_j = np.array([p_var_backbone(nLayers-1, p, path_dist).value for p in pGrid])
+if not os.path.isfile(fNameBase + "_pVar.npy"):
+    ## Compute p-variations of 1-dimensional sample path
+    path_dist = lambda k, l: np.abs(W[k,iIndex,jIndex] - W[l,iIndex,jIndex])
+    
+    pVars_i_j = np.array([p_var_backbone(nLayers-1, p, path_dist).value for p in pGrid])
+    
+    ## Compute the p-variation for the whole matrix, using a specific matrix norm
+    mNorm = 'fro'
+    path_dist = lambda k,l: np.linalg.norm(W[k,:,:] - W[l,:,:], mNorm)
+    
+    pVarsFro = np.array([p_var_backbone(nLayers-1, p, path_dist).value for p in pGrid])
+    
+    mNorm = 2
+    path_dist = lambda k,l: np.linalg.norm(W[k,:,:] - W[l,:,:], mNorm)
+    fName2 = fNameBase + "_pVar_2.npy"
+    
+    pVars2 = np.array([p_var_backbone(nLayers-1, p, path_dist).value for p in pGrid])
+    
+    with open(fNameBase + "_pVar.npy", "wb") as f:
+        np.save(f, pVars_i_j)
+        np.save(f, pVarsFro)
+        np.save(f, pVars2)
+else:
+    with open(fNameBase + "_pVar.npy", "rb") as f:
+        pVars_i_j = np.load(f)
+        pVarsFro = np.load(f)
+        pVars2 = np.load(f)
+        
+## Plot the p-variations
+plt.plot(pGrid, pVars_i_j, "r-")
+plt.xlabel("p")
+plt.title(f"p-Var of W[{iIndex},{jIndex}]")
+plt.show()
 
-with open(fName_i_j, "wb") as f:
-    np.save(f, pVars_i_j)
+plt.plot(pGrid, pVarsFro, "b-")
+plt.xlabel("p")
+plt.title("p-Var of W w.r.t. Frobenius norm")
+plt.show()
 
-## Compute the p-variation for the whole matrix, using a specific matrix norm
-mNorm = 'fro'
-pGrid = np.linspace(1.0, 3.0, 100)
-path_dist = lambda k,l: np.linalg.norm(W[k,:,:] - W[l,:,:], mNorm)
-fNameFro = fNameBase + "_pVar_fro.npy"
-
-pVarsFro = np.array([p_var_backbone(nLayers-1, p, path_dist).value for p in pGrid])
-
-with open(fNameFro, "wb") as f:
-    np.save(f, pVarsFro)
-
-mNorm = 2
-pGrid = np.linspace(1.0, 3.0, 100)
-path_dist = lambda k,l: np.linalg.norm(W[k,:,:] - W[l,:,:], mNorm)
-fName2 = fNameBase + "_pVar_2.npy"
-
-pVars2 = np.array([p_var_backbone(nLayers-1, p, path_dist).value for p in pGrid])
-with open(fName2, "wb") as f:
-    np.save(f, pVars2)
+plt.plot(pGrid, pVars2, "g-")
+plt.xlabel("p")
+plt.title("p-Var of W w.r.t. spectral norm")
+plt.show()
---
+++ iss_pvar.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 16 10:05:55 2021
+
+@author: bayerc
+"""
+import torch
+import numpy as np
+from iss2 import compute
+from p_var import p_var_backbone, p_var_backbone_ref
+
+def p_var_2(x, v_norm, m_norm, p):
+    """Compute the p-variation norm of the degree 2 part of the iterated sum
+    signature of a time series.
+    
+    Parameters
+    ----------
+    x : np.ndarray. Array of shape (M,d), representing a time series of M steps
+            with entries in R^d.
+    v_norm: function. A norm on R^d. Accepts torch.tensor.
+    m_norm : function. A matrix norm on R^{dxd}. norm is supposed to accept a
+            torch.tensor as argument.
+    p : float. A positive scalar.
+    
+    Output
+    ------
+    The (homogeneous) p-variation norms of the three components of the ISS of
+    degree 2."""
+    assert len(x.shape) == 2
+    M = x.shape[0]
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    X = torch.tensor(x).to(device)
+    ## Construct norm(S_{n,m}) for all n < m.
+    S0n_l = _get_S0n(X, range(1, M))
+    S0n_il = [g.inv() for g in S0n_l]
+    ## Now construct the norms of all ISS S_{i,j} for 0 <= i < j < M
+    chi = {} # empty dictionary for indexing
+    counter = 0
+    ## ndarrays for holding the norms
+    a_norms = np.full((M-1)*M/2, np.nan)
+    S_norms = np.full((M-1)*M/2, np.nan)
+    D_norms = np.full((M-1)*M/2, np.nan)
+    for j in range(1, M):
+        for i in range(j):
+            chi[(i,j)] = counter
+            Sij = S0n_l[j-1] if i == 0 else S0n_il[i-1] * S0n_l[j-1]
+            a_norms[counter] = v_norm(Sij.a)
+            S_norms[counter] = m_norm(Sij.S)
+            D_norms[counter] = m_norm(Sij.D)
+            counter = counter + 1
+    ## Now compute the p-variation norms
+    a_var = p_var_backbone_ref(M, p, lambda i,j: a_norms[chi[(i,j)]]) ** (1/p)
+    S_var = p_var_backbone_ref(M, p, lambda i,j: S_norms[chi[(i,j)]]) ** (1/p)
+    D_var = p_var_backbone_ref(M, p, lambda i,j: D_norms[chi[(i,j)]]) ** (1/p)
+    return a_var, S_var, D_var
+    
+def _get_S0n(X, n_list):
+    """Compute the ISS of X between step 0 and n for all n in n_list."""
+    return [compute(X[0:(n+1),:]) for n in n_list]
+
+def l2_norm(X):
+    """Compute the L2 norm of a general tensor X, i.e., the square root of the
+    sum of all squared entries."""
+    
