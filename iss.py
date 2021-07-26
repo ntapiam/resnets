767e8c10ba824c75a16d2c350b1573c5aeec1fb9
Nikolas Tapia <tapia@wias-berlin.de>
2021-07-26 14:22:30 +0200

    minor changes

--- iss.py
+++ iss.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     from tqdm import trange
 
     N = 100
-    d = 64 * 64
+    d = 64
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
     x = torch.randn(N, d, device=DEVICE).cumsum(axis=0) / np.sqrt(N) #generate d-dimensional brownian path
@@ -38,18 +38,22 @@ if __name__ == "__main__":
     print(f"Took {t1-t0:.>3f}s. Allocated {(sys.getsizeof(x.storage()) + sys.getsizeof(S.storage()))/ (1024 ** 2):.3f} MiB.")
 
     def level1_dist(j, k):
-        return torch.linalg.norm(x[k] - x[j])
+        return torch.linalg.norm(x[k] - x[j]).item()
 
     def level2_dist(j, k):
-        return torch.linalg.norm(S[k] - S[j] + torch.outer(x[j], x[j]), ord='fro')
+        return level1_dist(j,k) + torch.pow(torch.linalg.norm(S[k] - S[j] + torch.outer(x[j], x[j]), ord='fro'),0.5).item()
 
     ps = np.linspace(1, 3, 20)
     pvars = np.zeros(len(ps))
+    x, S = x.cpu(), S.cpu()
     print("Computing p-variations for p ∈ [1, 3]")
     for k in trange(len(ps)):
-        pvars[k] = pvar(N, ps[k], level1_dist, device=DEVICE)
-        if ps[k] > 2:
-            pvars[k] += torch.pow(pvar(N, ps[k]/2, level2_dist, device=DEVICE), 0.5)
+        dist = level1_dist if ps[k]<=2 else level2_dist
+        pvars[k] = pvar(N, ps[k], dist)
+
+    plt.plot(ps, pvars)
+    plt.xlabel('p')
+    plt.show()
 
     torch.save({'ps': ps, 'pvars': pvars}, 'pvar_data.pt')
     
