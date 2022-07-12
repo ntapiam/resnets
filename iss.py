147d2b1a0cce042c7cf458f5016249e2a775ccd8
Nikolas Tapia <tapia@wias-berlin.de>
2022-07-12 16:39:25 +0200

    load data

--- iss.py
+++ iss.py
@@ -1,5 +1,4 @@
 import torch
-import numpy as np
 
 
 def compute(x):
@@ -16,30 +15,23 @@ def compute(x):
     return S
 
 
-def pvar(n, p, d, device="cpu"):
-    running_pvar = torch.zeros(n, device=device)
-
-    for k in range(1, n):
-        dists = running_pvar[:k] + torch.pow(
-            torch.tensor([d(j, k) for j in range(k)], device=device), p
-        )
-        running_pvar[k] = dists.max()
-
-    return torch.pow(running_pvar[-1], 1 / p)
-
-
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
+    import seaborn as sns
     import time, sys
     from tqdm import trange
+    import itertools as it
+    import numpy as np
+    from p_var import p_var
+    from resnet import *
 
-    N = 100
-    d = 4096
-    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+    n_layers = 128
+    epochs = 100
 
-    x = torch.randn(N, d, device=DEVICE).cumsum(axis=0) / np.sqrt(
-        N
-    )  # generate d-dimensional brownian path
+    model = torch.load(f"resnet{n_layers}_relu_lin_e{epochs}.pth")
+    x = torch.zeros(n_layers, 64 * 64)
+    for (k, v) in enumerate(it.islice(model.parameters(), 8, 136)):
+        x[k] = v.flatten()
 
     t0 = time.perf_counter()
     S = compute(x)
@@ -47,45 +39,48 @@ if __name__ == "__main__":
     memory = (sys.getsizeof(x.storage()) + sys.getsizeof(S.storage())) / (1024 ** 3)
     print(f"Took {t1-t0:.>3f}s. Allocated {memory:.3f} GiB.")
 
-    print("Precomputing pairwise norms.")
-    t0 = time.perf_counter()
-    pairwise_x = torch.linalg.vector_norm(x.unsqueeze(0) - x.unsqueeze(1), dim=-1)
-    xout = (x - x[0]).unsqueeze(1) * (x - x[0]).unsqueeze(-1)
-    Sjk = torch.zeros(N, N)
+    # print("Precomputing pairwise norms.")
+    # t0 = time.perf_counter()
+    # pairwise_x = torch.linalg.vector_norm(x.unsqueeze(0) - x.unsqueeze(1), dim=-1)
+    # xout = (x - x[0]).unsqueeze(1) * (x - x[0]).unsqueeze(-1)
+    # Sjk = torch.zeros(N, N)
 
-    for j in range(N):
-        for k in range(N):
-            Sjk[j, k] = torch.pow(
-                torch.linalg.matrix_norm(S[k] - S[j] + xout[j], ord="fro"), 0.5
-            ).item()
+    # for j in range(N):
+    #     for k in range(N):
+    #         Sjk[j, k] = torch.pow(
+    #             torch.linalg.matrix_norm(S[k] - S[j] + xout[j], ord="fro"), 0.5
+    #         ).item()
 
-    t1 = time.perf_counter()
-    memory2 = (sys.getsizeof(pairwise_x.storage()) + sys.getsizeof(Sjk.storage())) / (
-        1024 ** 3
-    )
-    print(
-        f"Took {t1-t0:.>3f}s. Allocated {memory2:.>3f} GiB ({memory + memory2:.3f} GiB total)."
-    )
+    # t1 = time.perf_counter()
+    # memory2 = (sys.getsizeof(pairwise_x.storage()) + sys.getsizeof(Sjk.storage())) / (
+    #     1024 ** 3
+    # )
+    # print(
+    #     f"Took {t1-t0:.>3f}s. Allocated {memory2:.>3f} GiB ({memory + memory2:.3f} GiB total)."
+    # )
 
-    def level1_dist(j, k):
-        return pairwise_x[j, k]
+    # def level1_dist(j, k):
+    #     return pairwise_x[j, k]
 
-    def level2_dist(j, k):
-        return pairwise_x[j, k] + Sjk[j, k]
+    # def level2_dist(j, k):
+    #     return pairwise_x[j, k] + Sjk[j, k]
 
     ps = np.linspace(1, 3, 20)
     pvars = np.zeros(len(ps))
-    x, S = x.cpu(), S.cpu()
+    x, S = x.cpu(), S.cpu().reshape(S.shape[0], -1)
     print("Computing p-variations for p ∈ [1, 3]")
     t0 = time.perf_counter()
     for k in trange(len(ps)):
-        dist = level1_dist if ps[k] <= 2 else level2_dist
-        pvars[k] = pvar(N, ps[k], dist)
+        curr_pvar = p_var(x, ps[k], 'euclidean') ** (1 / ps[k])
+        if ps[k] >= 2:
+            curr_pvar += p_var(S, ps[k] / 2, 'euclidean') ** (2 / ps[k])
     t1 = time.perf_counter()
     print(f"Took {t1-t0:.>3f}s.")
 
+    sns.set_theme()
     plt.plot(ps, pvars)
     plt.xlabel("p")
     plt.show()
+    plt.savefig('pvar_plot.pdf')
 
     torch.save({"ps": ps, "pvars": pvars}, "pvar_data.pt")
